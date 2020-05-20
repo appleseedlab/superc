553c6b22c6b1233e3587b665c84f96f8c783ceca
Julian Braha <julianbraha@gmail.com>
2020-05-20 15:42:33 -0400

    Added DeclBuilder.toType() and .addType()
    DeclBuilder.toType() now returns a type object containing the type and and declaration information.
    Array lengths are not being stored.

--- src/xtc/lang/cpp/DeclBuilder.java
+++ src/xtc/lang/cpp/DeclBuilder.java
@@ -3,6 +3,11 @@ package xtc.lang.cpp;
 import java.util.LinkedList;
 import java.util.List;
 
+import xtc.type.Type;
+import xtc.type.UnitT;
+import xtc.type.PointerT;
+import xtc.type.ArrayT;
+
 public class DeclBuilder
 {
     String identifier;
@@ -10,110 +15,144 @@ public class DeclBuilder
     List<String> arrays;
     DeclBuilder inner;
     boolean isValid;
+    Type basicType;
+
+    // converts the declbuilder to a type object
+    public Type toType() {
+      if (!isValid) {
+        System.err.println("ERROR: attempting to create an invalid type.");
+        System.exit(1);
+      }
+
+      Type type;
+
+      // recursively gets the inner declbuilder information
+      if (inner == null)
+        type = basicType;
+      else
+        type = inner.toType();
+
+      for (String arr : arrays) {
+        if (arr.equals("")) { // array length has no expression
+          // NOTE: array length is internally being set to -1.
+          type = new ArrayT(type, false);
+        }
+        else if (arr.equals("const Expr")) { // array length is a constant expression
+          long length = -1; // TODO: possibly evaluate the constant expression, and set length to that
+          type = new ArrayT(type, length);
+        } else { // array length is a variable expression
+          // NOTE: array length is internally being set to -1.
+          type = new ArrayT(type, true);
+        }
+      }
+
+      // continually create new pointerT objects where each one points to the previous one
+      for (int i = 0; i < numPointers; i++) {
+        type = new PointerT(type);
+      }
+
+      return type;
+    }
 
     public DeclBuilder()
     {
-	identifier = "";
-	numPointers = 0;
-	arrays = new LinkedList<String>();
-	inner = null;
-	isValid = true;
+    	identifier = "";
+    	numPointers = 0;
+    	arrays = new LinkedList<String>();
+    	inner = null;
+    	isValid = true;
+      basicType = new UnitT();
     }
-    
+
     public DeclBuilder(String name)
     {
-	identifier = name;
-	numPointers = 0;
-	arrays = new LinkedList<String>();
-	inner = null;
-	isValid = true;
-    }
-    
-    public void addPointer()
-    {
-	numPointers++;
+    	identifier = name;
+    	numPointers = 0;
+    	arrays = new LinkedList<String>();
+    	inner = null;
+    	isValid = true;
+      basicType = new UnitT();
     }
 
-    public void addArray(String i)
-    {
-	addArray(i, true);
+    public void addPointer() {
+      numPointers++;
     }
 
-    public int countArrays()
-    {
-	int count = 0;
-        count += arrays.size();
-	if (inner != null)
+    public void addArray(String i) {
+	    addArray(i, true);
+    }
+
+    public int countArrays() {
+      int count = 0;
+      count += arrays.size();
+	    if (inner != null)
 	    count += inner.countArrays();
-	return count;
+	    return count;
     }
-    
-    public void addArray(String i, boolean seen)
-    {
-        if (!seen)
-	    if (countArrays() == 0)
-		arrays.add("");
+
+    public void addArray(String i, boolean seen) {
+      if (!seen)
+	       if (countArrays() == 0)
+		       arrays.add("");
+         else
+		       isValid = false;
+      else
+	       arrays.add(i);
+    }
+
+    public void addType(Type type) {
+      if (basicType instanceof UnitT)
+        basicType = type;
+      else // invalid: multiple types being added to the same declaration
+        isValid = false;
+    }
+
+    public void addDeclBuilder(DeclBuilder d) {
+	    inner = d;
+      if (!d.isValid)
+        isValid = false;
+    }
+
+    public void merge(DeclBuilder d) {
+      numPointers += d.numPointers;
+	    if (inner != null) {
+        if (d.inner != null)
+          isValid = false;
+      }
 	    else
-		isValid = false;
-        else
-	    arrays.add(i);
-	    
-    }
-
-    public void addDeclBuilder(DeclBuilder d)
-    {
-	inner = d;
-	if (!d.isValid)
-	    isValid = false;
-    }
-
-    public void merge(DeclBuilder d)
-    {
-	numPointers += d.numPointers;
-	if (inner != null)
-	    {
-		if (d.inner != null)
-		    isValid = false;
+        inner = d.inner;
+      if (!identifier.equals("")) {
+		     if (!d.identifier.equals(""))
+		       isValid = false;
 	    }
-	else
-	    inner = d.inner;
-	if (!identifier.equals(""))
-	    {
-		if (!d.identifier.equals(""))
-		     isValid = false;
-	    }
-	else
-	    identifier = d.identifier;
-	
-	for (int i = 0; i < d.arrays.size(); ++i)
-		arrays.add(d.arrays.get(i));	
+      else
+	      identifier = d.identifier;
+
+      for (int i = 0; i < d.arrays.size(); ++i)
+		    arrays.add(d.arrays.get(i));
     }
 
-    public String getID()
-    {
-	if (identifier.equals("") && inner != null)
-	    return inner.getID();
-	return identifier;
+    public String getID() {
+	    if (identifier.equals("") && inner != null)
+	      return inner.getID();
+      return identifier;
     }
 
-    public String toString()
-    {
-	if (!isValid)
-	    return "Decl Error";
-	String output = "";
-	for (int i = 0; i < numPointers; ++i)
-	    output += "*";
-	if (!identifier.equals(""))
-	    output += identifier;
-	if (inner != null)
-	    output += "(" + inner.toString() + ")";
-	for (int i = 0; i < arrays.size(); ++i)
-	    {
-		output += "[";
-		output += arrays.get(i);
-		output += "]";
+    public String toString() {
+	    if (!isValid)
+	      return "Decl Error";
+      String output = "";
+      for (int i = 0; i < numPointers; ++i)
+	      output += "*";
+      if (!identifier.equals(""))
+	      output += identifier;
+	    if (inner != null)
+	      output += "(" + inner.toString() + ")";
+	    for (int i = 0; i < arrays.size(); ++i) {
+		    output += "[";
+		    output += arrays.get(i);
+		    output += "]";
 	    }
-	return output;
+	    return output;
     }
-    
 }
