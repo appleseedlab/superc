48ac89a577d9476aa87a637b374bbea6204233e9
Paul <github@pgazz.com>
2020-05-29 16:00:27 -0400

    Created an abstract class for multiverses and a subclass for nodes

---
+++ src/xtc/lang/cpp/NodeMultiverse.java
@@ -0,0 +1,17 @@
+package xtc.lang.cpp;
+
+import java.util.List;
+
+import xtc.lang.cpp.PresenceConditionManager.PresenceCondition;
+import xtc.tree.Node;
+
+/**
+ * This is multiverse of AST Nodes.
+ *
+ * @author Paul Gazzillo
+ * @version $Revision: 1.272 $
+ */
+public class NodeMultiverse extends AbstractMultiverse<Node> {
+  public NodeMultiverse() {
+  }
+}
