12acb1bec0bf4684b14db175628b77df20242bc2
Paul <github@pgazz.com>
2020-06-29 18:37:08 -0400

    Add hand-computed symbol table conditions

--- fonda/cpp_testsuite/staticConfig/PASS_basic_pointer_array.c
+++ fonda/cpp_testsuite/staticConfig/PASS_basic_pointer_array.c
@@ -12,17 +12,28 @@ int main() {
 
 #ifdef C
 	arr;
+  // arr -> { int:A, char:!A & B, float:!A & !B
 #else
 	#ifdef D
 	arr[];
+  // arr -> { int:A & (C | !D), char:!A & B & (C | !D), float:!A & !B & (C | !D), int[]:A & !C & D, char[]:!A & B & !C & D, float[]:!A & !B & !C & D }
 	#else
 	*arr;
+  // arr -> { int:A & (C | !D) & (C | D), char:!A & B & (C | !D) & (C | D), float:!A & !B & (C | !D) & (C | D), int[]:A & !C & D & (C | D), char[]:!A & B & !C & D & (C | D), float[]:!A & !B & !C & D & (C | D), int*:A & !C & !D, char*:!A & B & !C & !D, float*:!A & !B & !C & !D }
 	#endif
 #endif
 
   #ifdef D
   char arr;
+  // arr -> { int:A & (C | !D) & (C | D) & !D, char:!A & B & (C | !D) & (C | D) & !D, float:!A & !B & (C | !D) & (C | D) & !D, int[]:A & !C & D & (C | D) & !D, char[]:!A & B & !C & D & (C | D) & !D, float[]:!A & !B & !C & D & (C | D) & !D, int*:A & !C & !D & !D, char*:!A & B & !C & !D & !D, float*:!A & !B & !C & !D & !D, char:D }
+  // arr -> { int:A & C & !D, char:!A & B & C & !D, float:!A & !B & C & !D, int[]:False, char[]:False, float[]:False, int*:A & !C & !D, char*:!A & B & !C & !D, float*:!A & !B & !C & !D, char:D }
   #endif
 
 	return 0;
 }
+
+
+
+(C | !D) & (C | D)
+
+C
