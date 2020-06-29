fcc9d4866f38f6d6359e34d78482cbace910d8be
pattersonz <pattersonz.se@gmail.com>
2020-06-29 11:56:43 -0500

    updated output of script

--- allTests.sh
+++ allTests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+rm allTests.out
+touch allTests.out
 cd fonda/cpp_testsuite
 folders="declChecker funcChecker staticConfig typechecker"
 for b in $folders;do
@@ -16,12 +18,20 @@ for b in $folders;do
 		elif [[ $f == "PASS"*".c" ]]; then
         clear
 			  java xtc.lang.cpp.SuperC "$f"
+        java xtc.lang.cpp.SuperC "$f" >> ../../../allTests.out
         echo ""
+        echo "" >> ../../../allTests.out
         echo "---file:"
+        echo "---file:" >> ../../../allTests.out
         echo ""
+        echo "" >> ../../../allTests.out
         cat "$f"
+        cat "$f" >> ../../../allTests.out
         echo "$b : $f"
-        read -s -n 1 -p "Press any key to continue . . ."
+        echo "$b : $f" >> ../../../allTests.out
+        if [[ $# == 1 && $1 == "-pause" && $f == *".c" ]]; then
+            read -s -n 1 -p "Press any key to continue . . ."
+        fi
 	  fi
 	done
   cd ..
