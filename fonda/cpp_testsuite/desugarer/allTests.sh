2dd7e2f46339a3436b12dbb9b5f7e4e2e5a61b5b
pattersonz <pattersonz.se@gmail.com>
2020-07-08 16:18:23 -0500

    updated test output location

--- fonda/cpp_testsuite/desugarer/allTests.sh
+++ fonda/cpp_testsuite/desugarer/allTests.sh
@@ -18,17 +18,17 @@ for b in $folders;do
 		elif [[ $f == "PASS"*".c" ]]; then
         clear
 			  java xtc.lang.cpp.SuperC "$f"
-        java xtc.lang.cpp.SuperC "$f" >> ../../../allTests.out 2>&1
+        java xtc.lang.cpp.SuperC "$f" >> ../../allTests.out 2>&1
         echo ""
-        echo "" >> ../../../allTests.out 2>&1
+        echo "" >> ../../allTests.out 2>&1
         echo "---file:"
-        echo "---file:" >> ../../../allTests.out 2>&1
+        echo "---file:" >> ../../allTests.out 2>&1
         echo ""
-        echo "" >> ../../../allTests.out 2>&1
+        echo "" >> ../../allTests.out 2>&1
         cat "$f"
-        cat "$f" >> ../../../allTests.out 2>&1
+        cat "$f" >> ../../allTests.out 2>&1
         echo "$b : $f"
-        echo "$b : $f" >> ../../../allTests.out 2>&1
+        echo "$b : $f" >> ../../allTests.out 2>&1
         if [[ $# == 1 && $1 == "-pause" && $f == *".c" ]]; then
             read -s -n 1 -p "Press any key to continue . . ."
         fi
