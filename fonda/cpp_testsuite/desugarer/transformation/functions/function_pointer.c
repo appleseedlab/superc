f456220d74e23ba009637ce498396785d1f4366e
Paul <github@pgazz.com>
2020-08-05 21:38:38 -0400

    Added tests for functions

---
+++ fonda/cpp_testsuite/desugarer/transformation/functions/function_pointer.c
@@ -0,0 +1,22 @@
+// from https://www.geeksforgeeks.org/function-pointer-in-c/
+
+void fun(int a) 
+{ 
+    printf("Value of a is %d\n", a); 
+} 
+  
+int main() 
+{ 
+    // fun_ptr is a pointer to function fun()  
+    void (*fun_ptr)(int) = &fun; 
+  
+    /* The above line is equivalent of following two 
+       void (*fun_ptr)(int); 
+       fun_ptr = &fun;  
+    */
+  
+    // Invoking fun() using fun_ptr 
+    (*fun_ptr)(10); 
+  
+    return 0; 
+} 
