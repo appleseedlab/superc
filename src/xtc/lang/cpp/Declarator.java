9da47f658fbb564bc668e1b648a133ff3f1b4b3b
Paul <github@pgazz.com>
2020-08-07 00:35:31 -0400

    Added the rest of the Declarator hierarchy
    Also implemented toString, renaming, and getType methods for all
    except qualified pointers, which are pending support from
    TypeBuilderUnit.

--- src/xtc/lang/cpp/Declarator.java
+++ src/xtc/lang/cpp/Declarator.java
@@ -3,6 +3,15 @@ package xtc.lang.cpp;
 import java.util.List;
 import java.util.LinkedList;
 
+import xtc.type.Type;
+import xtc.type.NumberT;
+import xtc.type.IntegerT;
+import xtc.type.FloatT;
+import xtc.type.VoidT;
+import xtc.type.PointerT;
+import xtc.type.ArrayT;
+import xtc.type.FunctionT;
+
 /**
  * The superclass of all C declarator constructs.
  *
@@ -21,8 +30,27 @@ import java.util.LinkedList;
  * declarators.
  */
 abstract class Declarator {
-  abstract public Declarator rename(String newName); // copy constructors except for simple
+  /**
+   * Get the identifier of this declarator, if any.  Abstract
+   * declarators do not have a name.
+   */
+  abstract public String getName();
 
+  /**
+   * Create a new declarator with the new name.  Abstract declarators
+   * do not have a name.
+   */
+  abstract public Declarator rename(String newName);
+
+  /**
+   * Create a new declarator with the new name.  Abstract declarators
+   * do not have a name.
+   */
+  abstract public Type getType(Type type);
+
+  /** Returns true if this is a EmptyDeclarator object. */
+  public boolean isEmptyDeclarator() { return false; }
+  
   /** Returns true if this is a SimpleDeclarator object. */
   public boolean isSimpleDeclarator() { return false; }
   
@@ -30,45 +58,56 @@ abstract class Declarator {
   public boolean isQualifiedPointerDeclarator() { return false; }
   
   /** Returns true if this is a PointerDeclarator object. */
+  public boolean isPointerAbstractDeclarator() { return false; }
+  
+  /** Returns true if this is a QualifiedPointerDeclarator object. */
+  public boolean isQualifiedPointerAbstractDeclarator() { return false; }
+  
+  /** Returns true if this is a PointerDeclarator object. */
   public boolean isPointerDeclarator() { return false; }
   
-  /** Returns true if this is a FunctionDeclarator object. */
+  /** Returns true if this is a FunctionPointerDeclarator object. */
   public boolean isFunctionDeclarator() { return false; }
   
-  /** Returns true if this is a FunctionPointerDeclarator object. */
-  public boolean isFunctionPointerDeclarator() { return false; }
+  /** Returns true if this is a FunctionDeclarator object. */
+  public boolean isParameterDeclarator() { return false; }
   
-  /** Returns true if this is a ArrayDeclarator object. */
+  /** Returns true if this is a FunctionDeclarator object. */
+  public boolean isParameterListDeclarator() { return false; }
+  
+  /** Returns true if this is a ArrayAbstractDeclarator object. */
   public boolean isArrayDeclarator() { return false; }
+  
+  /** Returns true if this is a ArrayAbstractDeclarator object. */
+  public boolean isArrayAbstractDeclarator() { return false; }
 
-  /** Casts declarator. */
-  public SimpleDeclarator toSimpleDeclarator() {
-    return (SimpleDeclarator) this;
-  }
+  /**
+   * This empty declarator is used for abstract declarators that only
+   * have a type.
+   */
+  public static class EmptyDeclarator extends Declarator {
+    public EmptyDeclarator() {
+    }
 
-  /** Casts declarator. */
-  public QualifiedPointerDeclarator toQualifiedPointerDeclarator() {
-    return (QualifiedPointerDeclarator) this;
-  }
+    public String getName() {
+      throw new IllegalStateException("abstract declarators have no name");
+    }
 
-  /** Casts declarator. */
-  public PointerDeclarator toPointerDeclarator() {
-    return (PointerDeclarator) this;
-  }
+    public Declarator rename(String newName) {
+      throw new IllegalStateException("abstract declarators have no name");
+    }
 
-  /** Casts declarator. */
-  public FunctionDeclarator toFunctionDeclarator() {
-    return (FunctionDeclarator) this;
-  }
+    public Type getType(Type type) {
+      throw new IllegalStateException("abstract declarators have no type");
+    }
 
-  /** Casts declarator. */
-  public FunctionPointerDeclarator toFunctionPointerDeclarator() {
-    return (FunctionPointerDeclarator) this;
-  }
+    @Override
+    public boolean isSimpleDeclarator() { return true; }
 
-  /** Casts declarator. */
-  public ArrayDeclarator toArrayDeclarator() {
-    return (ArrayDeclarator) this;
+    public String toString() {
+      // empty declarators have no string
+      return "";
+    }
   }
 
   /**
@@ -83,12 +122,24 @@ abstract class Declarator {
       this.name = name;
     }
 
+    public String getName() {
+      return name;
+    }
+
     public Declarator rename(String newName) {
       return new SimpleDeclarator(newName);
     }
 
+    public Type getType(Type type) {
+      return type;
+    }
+
     @Override
     public boolean isSimpleDeclarator() { return true; }
+
+    public String toString() {
+      return name;
+    }
   }
 
   /**
@@ -103,15 +154,26 @@ abstract class Declarator {
       this.declarator = declarator;
     }
 
+    public String getName() {
+      return declarator.getName();
+    }
+
     public Declarator rename(String newName) {
       return new PointerDeclarator(declarator.rename(newName));
+    }
+
+    public Type getType(Type type) {
+      return new PointerT(declarator.getType(type));
     }
   
     @Override
     public boolean isPointerDeclarator() { return true; }
+
+    public String toString() {
+      return String.format("(* %s)", declarator.toString());
+    }
   }
 
-  // note this uses TypeBuilderUnit, not a Multiverse, so the action needs to hoist, because TypeQualifiers returns a Multiverse
   /**
    * A qualified pointer declarator, which has another declarator nested
    * inside of it as well as a list of qualifiers.
@@ -120,7 +182,7 @@ abstract class Declarator {
     /** Points to this declarator. */
     protected final Declarator declarator;
 
-    /** Qualifiers, if any, of this pointer. */
+    /** Qualifiers of this pointer. */
     protected final TypeBuilderUnit qualifiers;
 
     public QualifiedPointerDeclarator(Declarator declarator,
@@ -129,21 +191,143 @@ abstract class Declarator {
       this.qualifiers = qualifiers;
     }
 
+    public String getName() {
+      return declarator.getName();
+    }
+
     public Declarator rename(String newName) {
       return new QualifiedPointerDeclarator(declarator.rename(newName), qualifiers);
+    }
+
+    public Type getType(Type type) {
+      System.err.println("WARNING: need to add qualifiers from typebuilderunit");
+      return new PointerT(declarator.getType(type));
     }
   
     @Override
     public boolean isQualifiedPointerDeclarator() { return true; }
+
+    public String toString() {
+      // TODO: waiting on typebuidlerunit tostring
+      throw new AssertionError("not yet implemented");
+    }
+  }
+
+  /**
+   * An abstract pointer declarator without an explicit declarator
+   * that it points to.  It has no member fields, because it is an
+   * abstract declarator.
+   */
+  public static class PointerAbstractDeclarator extends Declarator {
+    public PointerAbstractDeclarator() {
+    }
+
+    public String getName() {
+      throw new IllegalStateException("abstract declarators have no name");
+    }
+
+    public Declarator rename(String newName) {
+      // nothing to rename and its immutable, so return this class.
+      return this;
+    }
+
+    public Type getType(Type type) {
+      return new PointerT(type);
+    }
+  
+    @Override
+    public boolean isPointerAbstractDeclarator() { return true; }
+
+    public String toString() {
+      // TODO: double-check the position of parentheses around declarators
+      return "*";
+    }
+  }
+
+  // note this uses TypeBuilderUnit, not a Multiverse, so the action needs to hoist, because TypeQualifiers returns a Multiverse
+  /**
+   * A qualified pointer declarator without an explicit declarator
+   * that it points to.  It has no declarator field, because it is an
+   * abstract declarator.
+   */
+  public static class QualifiedPointerAbstractDeclarator extends Declarator {
+    /** Qualifiers, if any, of this pointer. */
+    protected final TypeBuilderUnit qualifiers;
+
+    public QualifiedPointerAbstractDeclarator(TypeBuilderUnit qualifiers) {
+      this.qualifiers = qualifiers;
+    }
+
+    public String getName() {
+      throw new IllegalStateException("abstract declarators have no name");
+    }
+
+    public Declarator rename(String newName) {
+      // nothing to rename and its immutable, so return this class.
+      return this;
+    }
+
+    public Type getType(Type type) {
+      System.err.println("WARNING: need to add qualifiers from typebuilderunit");
+      return new PointerT(type);
+    }
+  
+    @Override
+    public boolean isQualifiedPointerAbstractDeclarator() { return true; }
+
+    public String toString() {
+      // TODO: waiting on typebuidlerunit tostring
+      throw new AssertionError("not yet implemented");
+    }
+  }
+
+  /**
+   * An array declarator, which contains the declarator and an array
+   * abstract declarator.
+   */
+  public static class ArrayDeclarator extends Declarator {
+    /** The declarator that becomes an array. */
+    protected final Declarator declarator;
+
+    /** The array part of the declarator. */
+    protected final ArrayAbstractDeclarator arrayabstractdeclarator;
+
+    public ArrayDeclarator(Declarator declarator,
+                           ArrayAbstractDeclarator arrayabstractdeclarator) {
+      this.declarator = declarator;
+      this.arrayabstractdeclarator = arrayabstractdeclarator;
+    }
+
+    public String getName() {
+      return declarator.getName();
+    }
+
+    public Declarator rename(String newName) {
+      return new ArrayDeclarator(declarator.rename(newName),
+                                 (ArrayAbstractDeclarator) arrayabstractdeclarator.rename(newName));
+    }
+
+    public Type getType(Type type) {
+      // this an array of whatever the declarator is
+      return arrayabstractdeclarator.getType(declarator.getType(type));
+    }
+
+    @Override
+    public boolean isArrayDeclarator() { return true; }
+
+    public String toString() {
+      System.err.println("WARNING: do we need parentheses?");
+      return String.format("(%s%s)", declarator.toString(), arrayabstractdeclarator.toString());
+    }
   }
 
   // do we ever need to know the syntax of the array's expression?  if
   // so, we'd have to store an AST node instead.
   /**
-   * An array declarator, which contains the contents of the expression
-   * between its brackets.
+   * An array abstract declarator, which contains the contents of the
+   * expression between its brackets.
    */
-  public static class ArrayDeclarator extends Declarator {
+  public static class ArrayAbstractDeclarator extends Declarator {
     /**
      * The string contents of each array declarator.  A string will be
      * empty (but not null) if there is no expression between the
@@ -151,10 +335,35 @@ abstract class Declarator {
      */
     protected final List<StringBuilder> expressions;
 
-    public ArrayDeclarator(ArrayDeclarator arraydeclarator,
+    /**
+     * The initial single dimension array declarator.
+     */
+    public ArrayAbstractDeclarator(StringBuilder expression) {
+      this.expressions = new LinkedList();
+      this.expressions.add(expression);
+    }
+
+    /**
+     * Multi-dimensional array declarator.
+     */
+    public ArrayAbstractDeclarator(ArrayAbstractDeclarator arraydeclarator,
                            StringBuilder expression) {
       this.expressions = new LinkedList(arraydeclarator.expressions);
       this.expressions.add(expression);
+    }
+
+    public Type getType(Type type) {
+      Type arrayType = type;
+      assert expressions.size() > 0;  // otherwise no arraytype will be made
+      System.err.println("need to handle the expression to see if the array has a variable size of not");
+      for (StringBuilder expression : expressions) {
+        arrayType = new ArrayT(arrayType);
+      }
+      return arrayType;
+    }
+
+    public String getName() {
+      throw new IllegalStateException("abstract declarators have no name");
     }
 
     public Declarator rename(String newName) {
@@ -163,7 +372,17 @@ abstract class Declarator {
     }
 
     @Override
-    public boolean isArrayDeclarator() { return true; }
+    public boolean isArrayAbstractDeclarator() { return true; }
+
+    public String toString() {
+      StringBuilder sb = new StringBuilder();
+      assert expressions.size() > 0;  // otherwise no arraytype will be made
+      System.err.println("need to handle the expression to see if the array has a variable size of not");
+      for (StringBuilder expression : expressions) {
+        sb.append(String.format("[%s]", expression));
+      }
+      return sb.toString();
+    }
   }
 
   /**
@@ -171,53 +390,125 @@ abstract class Declarator {
    * parameters.
    */
   public static class FunctionDeclarator extends Declarator {
-    /** The declarators's identifier. */
-    protected final String name;
+    /** The declarator that becomes a function. */
+    protected final Declarator declarator;
 
-    /**
-     * The list of parameters for ths function declarator.  This list
-     * will be empty (but not null) if there are no parameters.
-     */  
-    protected final List<Parameter> parameters;
+    /** The function parameters. */
+    protected final ParameterListDeclarator parameters;
 
-    public FunctionDeclarator(String name, List<Parameter> parameters) {
-      this.name = name;
-      this.parameters = new LinkedList<Parameter>(parameters);
+    public FunctionDeclarator(Declarator declarator,
+                              ParameterListDeclarator parameters) {
+      this.declarator = declarator;
+      this.parameters = parameters;
+    }
+
+    public String getName() {
+      return declarator.getName();
     }
 
     public Declarator rename(String newName) {
-      return new FunctionDeclarator(newName, parameters);
+      // parameters don't affect the name of the declarator, so just
+      // use the original parameters
+      return new FunctionDeclarator(declarator.rename(newName),
+                                    parameters);
+    }
+
+    public Type getType(Type type) {
+      List<Type> paramtypes = new LinkedList<Type>();
+      boolean varargs = false;  // TODO: handle varargs here
+      for (ParameterDeclarator param : parameters.parameters) {
+        TypeBuilderUnit typebuilder = param.getType();
+        paramtypes.add(typebuilder.toType());
+      }
+      return new FunctionT(declarator.getType(type), paramtypes, varargs);
     }
   
     @Override
     public boolean isFunctionDeclarator() { return true; }
+
+    public String toString() {
+      return String.format("%s %s", declarator.toString(), parameters.toString());
+    }
   }
 
-  /**
-   * A function pointer declarator contains another declarator and a
-   * list of parameters.
-   */
-  public static class FunctionPointerDeclarator extends Declarator {
-    /** The pointer declarator. */
-    protected final Declarator declarator;
+  /** A parameter list. */
+  public static class ParameterListDeclarator extends Declarator {
+    /** The list of parameters. */
+    protected final List<ParameterDeclarator> parameters;
 
-    /**
-     * The list of parameters for ths function declarator.  This list
-     * will be empty (but not null) if there are no parameters.
-     */  
-    protected final List<Parameter> parameters;
+    public ParameterListDeclarator(List<ParameterDeclarator> parameters) {
+      this.parameters = parameters;
+    }
 
-    public FunctionPointerDeclarator(Declarator declarator,
-                                     List<Parameter> parameters) {
-      this.declarator = declarator;
-      this.parameters = new LinkedList<Parameter>(parameters);
+    public String getName() {
+      throw new IllegalStateException("parameters don't affect the name of the function");
     }
 
     public Declarator rename(String newName) {
-      return new FunctionPointerDeclarator(declarator.rename(newName), parameters);
+      throw new IllegalStateException("parameters don't affect the name of the function");
+    }
+
+    public Type getType(Type type) {
+      throw new IllegalStateException("Should only get parameter types via FunctionDeclarator");
     }
   
     @Override
-    public boolean isFunctionPointerDeclarator() { return true; }
+    public boolean isParameterListDeclarator() { return true; }
+
+    public String toString() {
+      StringBuilder sb = new StringBuilder();
+      sb.append("(");
+      String delim = "";
+      for (ParameterDeclarator param : parameters) {
+        sb.append(delim);
+        sb.append(param.toString());
+        delim = ", ";
+      }
+      sb.append(")");
+      return sb.toString();
+    }
+  }
+
+  /**
+   * A function declarator contains the function name and a list of
+   * parameters.
+   */
+  public static class ParameterDeclarator extends Declarator {
+    /** Type of the parameter. */
+    protected final TypeBuilderUnit type;
+
+    /** Type of the parameter. */
+    protected final Declarator declarator;
+
+    public ParameterDeclarator(TypeBuilderUnit type, Declarator declarator) {
+      this.type = type;
+      this.declarator = declarator;
+    }
+
+    public String getName() {
+      throw new IllegalStateException("parameters don't affect the name of the function");
+    }
+
+    public Declarator rename(String newName) {
+      throw new IllegalStateException("parameters don't affect the name of the function");
+    }
+
+    /**
+     * Return the typebuilder for this parameter.
+     */
+    public TypeBuilderUnit getType() {
+      return type;
+    }
+
+    public Type getType(Type type) {
+      throw new IllegalStateException("Should only get parameter types via FunctionDeclarator");
+    }
+  
+    @Override
+    public boolean isParameterDeclarator() { return true; }
+
+    public String toString() {
+      return String.format("%s %s", type.toString(), declarator.toString());
+    }
   }
 }
