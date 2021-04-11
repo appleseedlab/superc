a2d503c33ad6002fdae1b0d0c389a3b46a3d3fd8
Kaarthik <kaarthik.alagappan@gmail.com>
2021-04-10 22:29:17 -0400

    Adding support files
    Adding support files taken from cparser and modified for P4

---
+++ src/superc/p4parser/P4TokenCreator.java
@@ -0,0 +1,76 @@
+/*
+ * xtc - The eXTensible Compiler
+ * Copyright (C) 2009-2012 New York University
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+ * USA.
+ */
+package superc.p4parser;
+
+import java.io.StringReader;
+import java.io.IOException;
+
+import superc.core.TokenCreator;
+
+import superc.core.Syntax;
+import superc.core.Syntax.Kind;
+import superc.core.Syntax.Language;
+import superc.core.Syntax.Text;
+
+/**
+ * A token creator implementation for C.
+ *
+ * @author Paul Gazzillo
+ * @version $Revision: 1.6 $
+ */
+public class P4TokenCreator implements TokenCreator {
+
+  public Language<?> createStringLiteral(String str) {
+    return new Text<P4Tag>(P4Tag.STRING_LITERAL, str);
+  }
+
+  public Language<?> createIntegerConstant(int i) {
+    return new Text<P4Tag>(P4Tag.INTEGER, ((Integer) i).toString());
+  }
+
+  public Language<?> createIdentifier(String ident) {
+    return new Text<P4Tag>(P4Tag.IDENTIFIER, ident);
+  }
+
+  public Language<?> pasteTokens(Language<?> t1, Language<?> t2) {
+    // Paste the tokens and run the resulting string through the
+    // lexer.
+    String str = t1.getTokenText() + t2.getTokenText();
+    StringReader sr = new StringReader(str);
+    P4Lexer lexer = new P4Lexer(sr);
+    Syntax pasted;
+    Syntax next;
+
+    try {
+      pasted = lexer.yylex();
+      next = lexer.yylex();
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw new RuntimeException();
+    }
+
+    // If the token-paste was successful, the next token will be an
+    // EOF.
+    if (next.kind() == Kind.EOF) {
+      return pasted.toLanguage();
+    } else {
+      return null;
+    }
+  }
+}
