fd755e983aa52db223a308154efff34979ff6ead
SlaterLatiao <41390071+SlaterLatiao@users.noreply.github.com>
2021-08-05 09:53:49 -0500

    bug fix and further implementation of running Hercules

--- fonda/cpp_testsuite/desugarer/microbenchmark/tools/Hercules/tool.py
+++ fonda/cpp_testsuite/desugarer/microbenchmark/tools/Hercules/tool.py
@@ -10,28 +10,47 @@ class Tool:
     config = configparser.ConfigParser()
     config.read(os.path.join(tool_dir, 'Hercules.config'))
 
-    self.ifdeftodef_dir = config.get('paths', 'ifdeftoifDir')
+    self.working_dir = config.get('paths', 'workingDir')
     self.hercules = config.get('paths', 'hercules_script')
 
+    if os.path.exists(self.working_dir):
+      shutil.rmtree(self.working_dir)
+    os.mkdir(self.working_dir)
+    
+    self.test_dir = os.path.join(self.working_dir, 'test')
 
   def run(self, testcase: str):
-    if os.path.exists(self.ifdeftodef_dir):
-      shutil.rmtree(self.ifdeftodef_dir)
+    if os.path.exists(self.test_dir):
+      shutil.rmtree(self.test_dir)
+    os.mkdir(self.test_dir)
 
-    args = [self.hercules, testcase]
+    ifdeftoif_dir = os.path.join(self.working_dir, 'ifdeftoif')
+    if os.path.exists(ifdeftoif_dir):
+      shutil.rmtree(ifdeftoif_dir)
+
+    file_name = os.path.basename(testcase)
+    copied_test = os.path.join(self.test_dir, file_name)
+    shutil.copy2(testcase, copied_test)
+
+    os.chdir(self.test_dir)
+
+    args = [self.hercules, copied_test]
     hercules_code, hercules_out, err = run_command(args)
 
     # sugarC fails itself
     if hercules_code:
       return hercules_code
 
-    print(hercules_code)
-    print(hercules_out)
-    print(err)
+    ifdeftoif_file_name = file_name.replace('.c', '_ifdeftoif.c')
+    ifdeftoif_file_path = os.path.join(self.test_dir, ifdeftoif_file_name)
 
-    print(os.listdir(self.ifdeftodef_dir))
+    if not os.path.exists(ifdeftoif_file_path):
+      return 1
 
+    gcc_args = ['gcc', '-x', 'c', '-c', '-o', '/dev/null', ifdeftoif_file_path]
+    gcc_ret, _, _ = run_command(gcc_args)
 
+    return gcc_ret
 
   def clean(self):
-    return
+    shutil.rmtree(self.working_dir)
