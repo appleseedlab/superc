9261d6070b212e0ae1b97b155f195b19f3648f4d
pattersonz <pattersonz.se@gmail.com>
2022-10-22 12:39:50 -0500

    bug fix to static condition default change

--- src/superc/cdesugarer/CActions.java
+++ src/superc/cdesugarer/CActions.java
@@ -546,7 +546,7 @@ public class CActions implements SemanticActions {
             for (Element<String> prototypestr : subprototypestrmv) {
               sb.append(prototypestr.getData());
               sb.append(" //M:" + getCompoundRange(bodymv, prototypestr.getCondition()) + "\n");
-              sb.append(" {\n");
+              sb.append("{\n");
               sb.append(emitStatementDSV(bodymv, prototypestr.getCondition()));
               sb.append("}\n");
             }
@@ -11250,7 +11250,7 @@ public String staticInitialization(boolean showParseError) {
   // writes the extern declarations for the renamed preprocessor BDDs
   System.err.println("TODO: record original presence condition strings in file as well once raw strings are collected");
   for (Integer hash : condVars.keySet()) {
-    sb.append(String.format("extern const bool %s;\n", condVars.get(hash)));
+    sb.append(String.format("int %s() {return rand()%%2;};\n", condVars.get(hash)));
   }
 
   sb.append(String.format("%s {\n%s\n%s\n",
@@ -12636,7 +12636,7 @@ public String condToCVar(PresenceCondition cond) {
       String cvar = freshCId(String.format("static_condition_%s", unitUID));
       condVars.put(key, cvar);
       staticConditionRenamings.append(String.format("__static_condition_renaming(\"%s\", \"%s\");\n", cvar, cond.toString()));
-      return cvar;
+      return cvar+"()";
     }
   }
 }
